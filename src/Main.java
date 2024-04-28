8c4083e9ed2c107d6ae53152f95c3cf155ef5f4a
Cherryshine <jee020205@gmail.com>
2024-04-29 04:10:15 +0900

    1st push test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        int a = 1;
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int num1;
+        int num2;
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        num1 = sc.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        num2 = sc.nextInt();
+
+
+
     }
 }
